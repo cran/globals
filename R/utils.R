da6a296310e1e3142d711b0cdfc805f234601ac7
Henrik Bengtsson <henrikb@braju.com>
2015-06-11 01:36:14 +0000

    version 0.3.1

---
+++ R/utils.R
@@ -0,0 +1,31 @@
+asFunction <- function(expr, envir=parent.frame(), ...) {
+  eval(substitute(function() x, list(x=expr)), envir=envir, ...)
+}
+
+findBasePkgs <- local({
+  pkgs <- NULL
+  function() {
+    if (length(pkgs) > 0L) return(pkgs)
+    data <- installed.packages()
+    isBase <- (data[,"Priority"] %in% "base")
+    pkgs <<- rownames(data)[isBase]
+    pkgs
+  }
+})
+
+isBasePkgs <- function(pkgs) {
+  pkgs %in% findBasePkgs()
+}
+
+# cf. is.primitive()
+is.base <- function(x) {
+  if (typeof(x) != "closure") return(FALSE)
+  isBasePkgs(environmentName(environment(x)))
+}
+
+# cf. is.primitive()
+is.internal <- function(x) {
+  if (typeof(x) != "closure") return(FALSE)
+  body <- deparse(body(x))
+  any(grepl(".Internal", body, fixed=TRUE))
+}
